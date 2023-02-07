3919faf60e5508b3f6f8e811f029f9a0e13c98ac
“Eughan <2687794@student.gla.ac.uk>
2023-02-07 22:19:36 +0000

    Chpt 4: Page with Rango pic

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -106,6 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+STATICFILES_DIRS = [STATIC_DIR, ]
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
