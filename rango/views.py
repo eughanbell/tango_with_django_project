3618135219703297d3bbb95fd3b0ecf5c7c806ff
“Eughan <2687794@student.gla.ac.uk>
2023-02-10 02:07:32 +0100

    Chapter 9: Demo 2 Complete

--- rango/views.py
+++ rango/views.py
@@ -7,8 +7,29 @@ from django.shortcuts import redirect
 from django.urls import reverse
 from rango.forms import PageForm
 from rango.forms import UserForm, UserProfileForm
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
+
 
 # Create your views here.
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect(reverse('rango:index'))
+            else:
+                return HttpResponse("Your Rango account is disabled.")
+        else:
+            print(f"Invalid login details: {username}, {password}")
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request, 'rango/login.html')
+
+
 def register(request):
     registered = False
 
